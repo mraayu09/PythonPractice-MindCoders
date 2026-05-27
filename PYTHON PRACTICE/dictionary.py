f4fa022e4f37dcac88d8765c124a74e8e75722d2
AAYUSH BAIRAGI <aayushbairagi2891@gmail.com>
2026-05-27 17:03:59 +0530

    tuple

---
+++ PYTHON PRACTICE/dictionary.py
@@ -0,0 +1,15 @@
+# dictionary = {
+#                 "cat": "chat",
+#                 "dog": "chien",
+#                 "horse": "cheval"
+#                 }
+# phone_numbers = {'boss': 5551234567, 'Suzy': 22657854310}
+# empty_dictionary = {}
+
+# print(dictionary)
+# print(type(dictionary))
+# print(phone_numbers)
+# print(type(phone_numbers))
+# print(empty_dictionary)
+# print(type(empty_dictionary))
+#-------------------------------------------------------------
