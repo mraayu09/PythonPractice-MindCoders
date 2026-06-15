6badd3e44afcf2b11543438412cd8d997a9a606b
AAYUSH BAIRAGI <aayushbairagi2891@gmail.com>
2026-06-15 16:25:15 +0530

    numpy library

--- PYTHON PRACTICE/my_numpy.py
+++ PYTHON PRACTICE/my_numpy.py
@@ -1,5 +1,5 @@
 import numpy as np
-#create array
+# #create array
 
 arr1d = np.array([1, 2, 3, 4, 5])
 arr2d = np.array([[85,90,78],[72,88,95],[91,76,83]])
@@ -7,3 +7,43 @@ arr2d = np.array([[85,90,78],[72,88,95],[91,76,83]])
 print(arr2d.shape)
 print(arr2d.dtype)
 print(arr2d.ndim)
+print("=====================================")
+
+zeros=np.zeros((3,4))
+print(zeros)
+print("=====================================")
+
+ones = np.ones((2,5))
+print(ones)
+print("=====================================")
+
+arange=np.arange(0,50,5)
+print(arange)
+print("=====================================")
+
+linspace=np.linspace(0,1,11)
+print(linspace)
+print("=====================================")
+
+random=np.random.randint(40,100,(5,3))
+print(random)
+print("=====================================")
+
+array=np.array([10,20,30,40,50])
+print(array*2)
+print(array+5)
+print(array**2)
+print("=====================================")
+
+marks_2d=np.array([[85,90,78],[72,88,95],[91,76,83]])
+print(np.mean(marks_2d))    #mean of whole array
+print(np.mean(marks_2d,axis=1))   #mean of row
+print(np.mean(marks_2d,axis=0))   #mean per column
+print(np.max(marks_2d))       #max num. of 3=2d array
+print(np.std(marks_2d))      #std daviation
+print("=====================================")
+
+array = np.array([55,82,43,91,67,78,35,83])
+print(array[array>70])
+print("=====================================")
+
