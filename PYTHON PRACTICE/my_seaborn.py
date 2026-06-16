7e4471cc33ff072ce299d233c1fd0136f74a5792
AAYUSH BAIRAGI <aayushbairagi2891@gmail.com>
2026-06-16 17:59:10 +0530

    seaborn

---
+++ PYTHON PRACTICE/my_seaborn.py
@@ -0,0 +1,17 @@
+import seaborn as sns
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+# np.random.seed(42)
+# #data
+# df = pd.DataFrame({
+#     'marks': np.random.randint(40,100,100),
+#     'study_hours': np.random.uniform(2,10,100),
+#     'city': np.random.choice(['Bhopal','Indore','Jabalpur'],100),
+#     'gender': np.random.choice(['Male','Female'],100)})
+
+# plt.figure(figsize=(10,4))
+# sns.histplot(df['marks'], bins=20, kde=True, color='steelblue')
+# plt.title('Distribution of students marks')
+# plt.show()
